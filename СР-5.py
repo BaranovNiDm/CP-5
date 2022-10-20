0b077daed3ebc1d143f13199199a23d02909632f
NikitaBaranov <dimawarriors3qazwsx@gmail.com>
2022-10-21 00:28:30 +0300

    dz

---
+++ СР-5.py
@@ -0,0 +1,34 @@
+b=0
+i=0
+chis10=int(input("введите число в десятичной системе счисления: "))
+while b<1:
+    sys=int(input("введите систему счисления (2 или 8): "))
+    if int(sys)==2 or int(sys)==8:
+        break
+    else:
+        print('пожалуйста, не пытайтесь ввести не 2 или 8')
+        continue
+a=[]
+chis10=int(chis10)
+while chis10 > 0:
+    a.append(int(chis10%sys))
+    chis10=chis10//sys
+while len(a)<8:
+    a.append(0)
+r=reversed(a)
+konecnoe=''
+for i in range(1,len(a)+1):
+    konecnoe=konecnoe+str(a[-i])
+print(konecnoe)
+
+
+
+
+
+
+    
+
+  
+        
+    
+        
